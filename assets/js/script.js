6372ee759ecfbf9461389c22dc2e84b9fb2f84ef
Lily Thomas <lilylynettethomas@gmail.com>
2022-01-07 23:19:06 -0800

    finished js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,7 +13,6 @@ $(document).ready(function () {
         // Get items in local storage
         localStorage.setItem(time, text);
     })
-    //load any saved data from LocalStorage - do this for each hour created.
     $("#hour9 .description").val(localStorage.getItem("hour9"));
     $("#hour10 .description").val(localStorage.getItem("hour10"));
     $("#hour11 .description").val(localStorage.getItem("hour11"));
@@ -23,3 +22,34 @@ $(document).ready(function () {
     $("#hour15 .description").val(localStorage.getItem("hour15"));
     $("#hour16 .description").val(localStorage.getItem("hour16"));
     $("#hour17 .description").val(localStorage.getItem("hour17"));
+
+
+    function hourTracker() {
+        //get current number of hours.
+        var currentHour = moment().hour();
+
+        // loop over time blocks
+        $(".time-block").each(function () {
+            var blockHour = parseInt($(this).attr("id").split("hour")[1]);
+            console.log( blockHour, currentHour)
+
+            //check if we've moved past this time
+            if (blockHour < currentHour) {
+                $(this).addClass("past");
+                $(this).removeClass("future");
+                $(this).removeClass("present");
+            }
+            else if (blockHour === currentHour) {
+                $(this).removeClass("past");
+                $(this).addClass("present");
+                $(this).removeClass("future");
+            }
+            else {
+                $(this).removeClass("present");
+                $(this).removeClass("past");
+                $(this).addClass("future");
+            }
+        })
+    }
+    hourTracker();
+})
