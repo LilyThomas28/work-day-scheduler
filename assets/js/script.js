0830503dd63bfa296a0f3dc00136a75af9aa99f6
Lily Thomas <lilylynettethomas@gmail.com>
2022-01-07 23:18:09 -0800

    set every hour to save in local stoarge

---
+++ assets/js/script.js
@@ -0,0 +1,25 @@
+// Tells engine to load html and css first
+$(document).ready(function () {
+    // Display current day and time
+    $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
+
+    // Assign saveBtn click listener for user input and time stamp??
+    $(".saveBtn").on("click", function () {
+        // Get nearby values
+        console.log(this);
+        var text = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
+
+        // Get items in local storage
+        localStorage.setItem(time, text);
+    })
+    //load any saved data from LocalStorage - do this for each hour created.
+    $("#hour9 .description").val(localStorage.getItem("hour9"));
+    $("#hour10 .description").val(localStorage.getItem("hour10"));
+    $("#hour11 .description").val(localStorage.getItem("hour11"));
+    $("#hour12 .description").val(localStorage.getItem("hour12"));
+    $("#hour13 .description").val(localStorage.getItem("hour13"));
+    $("#hour14 .description").val(localStorage.getItem("hour14"));
+    $("#hour15 .description").val(localStorage.getItem("hour15"));
+    $("#hour16 .description").val(localStorage.getItem("hour16"));
+    $("#hour17 .description").val(localStorage.getItem("hour17"));
